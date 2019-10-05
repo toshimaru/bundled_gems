686c70a001dc83cf718d393f3f4f973873994ad1
toshimaru <me@toshimaru.net>
2019-10-06 06:31:57 +0900

    `::Bundler`

--- lib/bundled_gem/cli.rb
+++ lib/bundled_gem/cli.rb
@@ -7,7 +7,7 @@ module BundledGem
     desc "list", "bundle list(without bundle install)"
     def list
       gemfilelock_content = File.read("./Gemfile.lock")
-      lockfile = Bundler::LockfileParser.new(gemfilelock_content)
+      lockfile = ::Bundler::LockfileParser.new(gemfilelock_content)
       lockfile.specs.each { |s| puts "#{s.name}, #{s.version}" }
     end
   end
