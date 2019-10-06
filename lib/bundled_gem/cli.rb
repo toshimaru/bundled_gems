cab1b1ba6ce25f84e3f8624b02981a735f7a75de
toshimaru <me@toshimaru.net>
2019-10-06 20:29:49 +0900

    Add default value

--- lib/bundled_gem/cli.rb
+++ lib/bundled_gem/cli.rb
@@ -5,9 +5,9 @@ require 'thor'
 module BundledGem
   class Cli < Thor
     desc "install [BUNDLED_GEM]", "install [BUNDLED_GEM] from `Gemfile.lock`"
-    option "lockfile", type: :string, desc: "Use the specified gemfile.lock instead of Gemfile.lock"
+    option "lockfile", type: :string, default: LockfileReader::LOCKFILE, desc: "Use the specified gemfile.lock instead of Gemfile.lock"
     def install(bundled_gem)
-      reader = LockfileReader.new
+      reader = LockfileReader.new(lockfile: options[:lockfile])
       if reader.gem_listed?(bundled_gem)
         version = reader.get_version(bundled_gem)
         system "gem install #{bundled_gem} --version #{version}"
