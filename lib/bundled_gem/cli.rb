182752be7a3f109ee1f74657276bfb47acc26b59
toshimaru <me@toshimaru.net>
2019-10-06 14:05:13 +0900

    Add install command

--- lib/bundled_gem/cli.rb
+++ lib/bundled_gem/cli.rb
@@ -4,7 +4,12 @@ require 'thor'
 
 module BundledGem
   class Cli < Thor
-    desc "list", "bundle list(without bundle install)"
+    desc "install [BUNDLED_GEM]", "install [BUNDLED_GEM] from `Gemfile.lock` without `bundle install`"
+    def install(bundled_gem)
+      # TODO
+    end
+
+    desc "list", "bundle list without `bundle install`"
     def list
       gemfilelock_content = File.read("./Gemfile.lock")
       lockfile = ::Bundler::LockfileParser.new(gemfilelock_content)
