ea44ac2b7f9547cc2cbae7ed6624685edfa4639a
Toshimaru <me@toshimaru.net>
2019-10-09 00:03:30 +0900

    Add Thor CLI Test (#8)
    * Test: test_list
    
    * Add test: test_unknown_command
    
    * Add test: test_install
    
    * Add install gem test
    
    * Update README.md

---
+++ test/bundled_gem/cli_test.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require "test_helper"
+require 'bundled_gem/cli'
+
+module BundledGem
+  class CliTest < Minitest::Test
+    def test_intall_with_no_arg
+      out, err = capture_io { BundledGem::Cli.start(['install']) }
+      assert_empty out
+      assert_match(/was called with no arguments/, err)
+      assert_match(/install \[BUNDLED_GEM\]/, err)
+    end
+
+    def test_intall_with_arg
+      out, err = capture_io { BundledGem::Cli.start(['install', 'rake']) }
+      assert_empty out
+      assert_empty err
+    end
+
+    def test_list
+      out, err = capture_io { BundledGem::Cli.start(['list']) }
+      assert_empty err
+      assert_match(/Gems included in `Gemfile.lock`:/, out)
+    end
+
+    def test_unknown_command
+      out, err = capture_io { BundledGem::Cli.start(['unknown']) }
+      assert_empty out
+      assert_match(/Could not find command "unknown"/, err)
+    end
+  end
+end
