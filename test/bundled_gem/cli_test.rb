9e8a9c658fc973b930ea3fa0cddb4d6c3c45d5a2
toshimaru <me@toshimaru.net>
2019-10-08 23:15:52 +0900

    Add test: test_install

--- test/bundled_gem/cli_test.rb
+++ test/bundled_gem/cli_test.rb
@@ -6,18 +6,22 @@ require 'bundled_gem/cli'
 module BundledGem
   class CliTest < Minitest::Test
     def test_intall
+      out, err = capture_io { BundledGem::Cli.start(['install']) }
+      assert_empty out
+      assert_match(/was called with no arguments/, err)
+      assert_match(/install \[BUNDLED_GEM\]/, err)
     end
 
     def test_list
       out, err = capture_io { BundledGem::Cli.start(['list']) }
       assert_empty err
-      assert out.include?("Gems included in `Gemfile.lock`:")
+      assert_match(/Gems included in `Gemfile.lock`:/, out)
     end
 
     def test_unknown_command
       out, err = capture_io { BundledGem::Cli.start(['unknown']) }
       assert_empty out
-      assert err.include?('Could not find command "unknown"')
+      assert_match(/Could not find command "unknown"/, err)
     end
   end
 end
