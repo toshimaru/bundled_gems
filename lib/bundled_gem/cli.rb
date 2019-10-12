1550c34dec109a65b9d1eef3161c6dd3710a4696
toshimaru <me@toshimaru.net>
2019-10-12 19:04:08 +0900

    Allow multiple gems for install command

--- lib/bundled_gem/cli.rb
+++ lib/bundled_gem/cli.rb
@@ -7,13 +7,15 @@ module BundledGem
   class Cli < Thor
     desc "install [BUNDLED_GEM]", "install [BUNDLED_GEM] from `Gemfile.lock`"
     option "lockfile", type: :string, default: LOCKFILE, desc: "Use the specified gemfile.lock instead of Gemfile.lock"
-    def install(bundled_gem)
+    def install(*bundled_gems)
       reader = LockfileReader.new(lockfile: options[:lockfile])
-      if reader.gem_listed?(bundled_gem)
-        version = reader.get_version(bundled_gem)
-        system "gem install #{bundled_gem} --version #{version}"
-      else
-        abort "`#{bundled_gem}` is not listed in Gemfile.lock." 
+      bundled_gems.each do |bundled_gem|
+        if reader.gem_listed?(bundled_gem)
+          version = reader.get_version(bundled_gem)
+          system "gem install #{bundled_gem} --version #{version}"
+        else
+          warn "`#{bundled_gem}` is not listed in Gemfile.lock." 
+        end
       end
     end
 
