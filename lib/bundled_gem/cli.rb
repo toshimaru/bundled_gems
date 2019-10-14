092b465840faf744df2a781c8d407d2dd0defd22
toshimaru <me@toshimaru.net>
2019-10-14 09:46:39 +0900

    rubocop -a lib

--- lib/bundled_gem/cli.rb
+++ lib/bundled_gem/cli.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require 'open3'
-require 'thor'
+require "open3"
+require "thor"
 
 module BundledGem
   # bundled_gem CLI class powered by thor
@@ -22,7 +22,7 @@ module BundledGem
             end
           end
         else
-          warn "`#{bundled_gem}` is not listed in Gemfile.lock." 
+          warn "`#{bundled_gem}` is not listed in Gemfile.lock."
         end
       end
     end
@@ -31,8 +31,8 @@ module BundledGem
     option "lockfile", type: :string, default: LOCKFILE, desc: "Use the specified gemfile.lock instead of Gemfile.lock"
     def list
       puts "Gems included in `#{options[:lockfile]}`:"
-      LockfileReader.new(lockfile: options[:lockfile]).lockfile_specs.each do |spec| 
-        puts "  * #{spec.name}, #{spec.version}" 
+      LockfileReader.new(lockfile: options[:lockfile]).lockfile_specs.each do |spec|
+        puts "  * #{spec.name}, #{spec.version}"
       end
     end
 
