0bf565278a4d6d39bcbd68f4c96c86500fc12928
toshimaru <me@toshimaru.net>
2019-10-14 09:45:01 +0900

    rubocop -a test

--- test/bundled_gem/cli_test.rb
+++ test/bundled_gem/cli_test.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 require "test_helper"
-require 'bundled_gem/cli'
+require "bundled_gem/cli"
 
 module BundledGem
   class CliTest < Minitest::Test
     def test_intall_with_no_arg
       out, err = capture_io do
         begin
-          BundledGem::Cli.start(['install']) 
+          BundledGem::Cli.start(["install"])
         rescue SystemExit
         end
       end
@@ -17,20 +17,20 @@ module BundledGem
     end
 
     def test_intall_single_gem
-      out, err = capture_io { BundledGem::Cli.start(['install', 'rake']) }
+      out, err = capture_io { BundledGem::Cli.start(["install", "rake"]) }
       assert_match(/Successfully installed rake/, out)
       assert_empty err
     end
 
     def test_intall_multiple_gems
-      out, err = capture_io { BundledGem::Cli.start(['install', 'rake', 'minitest']) }
+      out, err = capture_io { BundledGem::Cli.start(["install", "rake", "minitest"]) }
       assert_match(/Successfully installed rake/, out)
       assert_match(/Successfully installed minitest/, out)
       assert_empty err
     end
 
     def test_intall_invalid_gems
-      out, err = capture_io { BundledGem::Cli.start(['install', 'a', 'b', 'c']) }
+      out, err = capture_io { BundledGem::Cli.start(["install", "a", "b", "c"]) }
       assert_empty out
       assert_match(/`a` is not listed in Gemfile.lock./, err)
       assert_match(/`b` is not listed in Gemfile.lock./, err)
@@ -38,20 +38,20 @@ module BundledGem
     end
 
     def test_intall_valid_and_invalid_gems
-      out, err = capture_io { BundledGem::Cli.start(['install', 'a', 'rake', 'b']) }
+      out, err = capture_io { BundledGem::Cli.start(["install", "a", "rake", "b"]) }
       assert_match(/Successfully installed rake/, out)
       assert_match(/`a` is not listed in Gemfile.lock./, err)
       assert_match(/`b` is not listed in Gemfile.lock./, err)
     end
 
     def test_list
-      out, err = capture_io { BundledGem::Cli.start(['list']) }
+      out, err = capture_io { BundledGem::Cli.start(["list"]) }
       assert_empty err
       assert_match(/Gems included in `Gemfile.lock`:/, out)
     end
 
     def test_unknown_command
-      out, err = capture_io { BundledGem::Cli.start(['unknown']) }
+      out, err = capture_io { BundledGem::Cli.start(["unknown"]) }
       assert_empty out
       assert_match(/Could not find command "unknown"/, err)
     end
